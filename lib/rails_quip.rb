db7afbc2d1be6ff432c10b1683f9bd882acb2350
qinmingyuan <mingyuan0715@foxmail.com>
2021-05-24 23:57:30 +0800

    quip files

--- lib/rails_quip.rb
+++ lib/rails_quip.rb
@@ -2,3 +2,14 @@ require 'rails_quip/engine'
 require 'rails_quip/config'
 require 'quip'
 
+module Quip
+
+  def self.use_relative_model_naming?
+    true
+  end
+
+  def self.table_name_prefix
+    'quip_'
+  end
+
+end
